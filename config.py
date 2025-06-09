4d977805efb13b7622e9aeb21065916106dd46ab
sandisir <sandisir741@gmail.com>
2025-06-10 00:09:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ API_HASH = os.environ.get("API_HASH", "7545825d90eb7adae543d59909c73121")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "8154916973:AAEJP8r_R4nfNnHlCd-4yv5AD7IBheod7Kw")
 
 OWNER_ID = int(os.environ.get("OWNER_ID", "5543709855"))  # Your Telegram user ID
-SUDO_USERS = list(map(int, os.environ.get("SUDO_USERS", "").split()))  # Space-separated user IDs
+SUDO_USERS = list(map(int, os.environ.get("SUDO_USERS", "5543709855").split()))  # Space-separated user IDs
 
 MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://sandeep123:<db_password>@cluster0.ggtof.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")##your mongo url eg: withmongodb+srv://xxxxxxx:xxxxxxx@clusterX.xxxx.mongodb.net/?retryWrites=true&w=majority
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-"))  # Telegram channel ID (with -100 prefix)
